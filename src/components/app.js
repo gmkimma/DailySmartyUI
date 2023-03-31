c51e776adb725c713db8ffa6bb4c0140cdafe09f
Greg Kimma <gmkimma@gmail.com>
2023-03-30 17:38:02 -0700

    setup logo component and corresponding styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import Logo from './logo'
 
 export default class App extends Component {
   render () {
     return (
-      <div className='app'>
-        <div>logo</div>
+      <div className='container'>
+        <div class='logo'>
+          <Logo />
+        </div>
+
+        <div class='search'>
+          <input placeholder='daily smarty' />
+        </div>
+
+        <div class='recent'>
+          <ul>
+            <li>post</li>
+            <li>post</li>
+            <li>post</li>
+          </ul>
+        </div>
       </div>
     )
   }
